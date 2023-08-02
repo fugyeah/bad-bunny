72fd11804196dc4b5c04c6643c7307a0051d33ff
Fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-08-02 18:45:16 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,7 @@ import Form from './components/main-form';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-24">
-      <div className="w-full flex items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black mb-4">
-        <a
-          className="flex place-items-center gap-2 p-8"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="p-8 bg-transparent w-full sm:w-3/4 md:w-1/2 lg:w-1/3 object-contain mx-auto">
-            <Image
-              src="/goodrabbit_logo.png"
-              alt="Wish you were Here"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </div>
-        </a>
-      </div>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-2xl text-center">
   Send a e-postcard to the Governor telling him how much we miss him in Florida!</p>
 
