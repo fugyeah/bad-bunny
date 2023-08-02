87f14b9c147aa2381ea17a92e7e1f155b9fc20f7
Fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-08-02 16:43:33 -0400

    move photo back to top

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,24 @@ import Form from './components/main-form';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-24">
+      <div className="w-full flex items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black mb-6">
+        <a
+          className="flex place-items-center gap-2 p-8"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="p-8 bg-transparent w-full sm:w-3/4 md:w-1/2 lg:w-1/3 object-contain mx-auto">
+            <Image
+              src="/WishYouWereHere.png"
+              alt="Wish you were Here"
+              width={897}
+              height={119}
+              priority
+            />
+          </div>
+        </a>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-2xl text-center">
   Send a e-postcard to the Governor telling him how much we miss him in Florida!</p>
@@ -11,7 +29,7 @@ export default function Home() {
         <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         </div>
 
-      </div>         
+      </div>        
        
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -87,24 +105,6 @@ export default function Home() {
       </div>
      <div className="w-full sm:w-10/12 md:w-10/12 lg:w-10/12">
         A service of HelloFLA as a demonstration of web app that automatically customizes each constituent letter on your issue advocacy campaigns using GPT! Copyright 2023 Painted Dog Productions, LLC
-      </div>
-      <div className="w-full flex items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
-        <a
-          className="flex place-items-center gap-2 p-8"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="p-8 bg-transparent w-full sm:w-3/4 md:w-1/2 lg:w-1/3 object-contain">
-            <Image
-              src="/WishYouWereHere.png"
-              alt="Wish you were Here"
-              width={897}
-              height={119}
-              priority
-            />
-          </div>
-        </a>
       </div>
     </main>
   )
