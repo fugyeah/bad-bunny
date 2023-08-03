95531f66863f670b0476725f291c6c01a65f5de4
Fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-08-03 10:04:38 -0400

    Update layout.js
    Signed-off-by: Fugyeah <30422405+fugyeah@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <Head>
+  <title>{metadata.title}</title>
+  <meta name="description" content={metadata.description} />
+  <meta property="og:image" content="https://www.hellofla.com/_next/image?url=%2FWishYouWereHere.png&w=384&q=75&dpl=dpl_4fCWHY7Kdrz79ko1qynLKRYz9ATE" />
+  <link rel="icon" href="https://www.hellofla.com/_next/image?url=%2FWishYouWereHere.png&w=384&q=75&dpl=dpl_4fCWHY7Kdrz79ko1qynLKRYz9ATE" />
+</Head>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
