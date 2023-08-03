4ea219f41d1f8ed2eb8d45d88bd432871afea404
Fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-08-03 07:54:06 -0400

    Color Match
    The color is set to #00FF00 (green), the background color is set to #000000 (black), and the border color is also set to #00FF00 (green).

--- src/app/components/main-form.js
+++ src/app/components/main-form.js
@@ -71,8 +71,8 @@ export default function Form() {
     setIsGenerating(false);
   }
 
-  return(
-    <div className="max-w-7x1 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto sm:px-6 lg:px-8 py-12">
+ return(
+    <div className="max-w-7x1 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto sm:px-6 lg:px-8 py-12" style={{ backgroundColor: '#000000', padding: '10px', borderRadius: '10px', boxShadow: '0px 0px 20px rgba(0,0,0,0.5)', borderBottom: '2px solid #00FF00' }}>
       <div className="grid gap-y-12 md:grid-cols-1 md:gap-x-12 ">
         <form onSubmit={handleSubmit} className="grid gap-y-5 text-amber-700 mx-2">
           <div className="flex flex-col">
@@ -86,9 +86,10 @@ export default function Form() {
               placeholder="Your Name (Required)"
               onChange={handleInput}
               className="px-2 sm:px-3 md:px-4 lg:px-5 block w-full sm:w-3/4 md:w-1/2 lg:w-1/3 rounded-md bg-white border border-gray-40 shadow-s focus:outline-none focus:outline-amber-300"
+              style={{ color: '#00FF00', backgroundColor: '#000000', borderColor: '#00FF00' }}
             />
           </div>
-        <div className="flex flex-col">
+          <div className="flex flex-col">
             <label className="sr-only" htmlFor="senderEmail">
               Your Email:
             </label>
@@ -101,40 +102,46 @@ export default function Form() {
               placeholder="Your Email (Required)"
               onChange={handleInput}
               className="px-2 block w-full rounded-md bg-white border border-gray-40 shadow-s focus:outline-none focus:outline-amber-300"
+              style={{ color: '#00FF00', backgroundColor: '#000000', borderColor: '#00FF00' }}
             />
           </div>
-        <div className="flex flex-col">
-          Recipient's Name: Governor Ron DeSantis</div>
-          <div className="flex flex-col">Their Email: GovernorRon.DeSantis@eog.myflorida.com
-        </div>
-        <div className="flex flex-col">
-          <label className="sr-only" htmlFor="extra">Anything you want to ADD? (Optional)</label>
-          <textarea
-            rows={7}
-            maxLength={200}
-            id="extra"
-            name="extra"
-            value={formData.extra}
-            placeholder="Anything you want to ADD? (Optional)"
-            onChange={handleInput}
-            className="px-2 py-0.5 block w-full rounded-md bg-white border border-gray-40 shadow-s focus:outline-none focus:outline-amber-300"
-          />
-          <div className="text-slate-400 text-right py-2">{characterCount} / 200</div>
-        </div>
-        <button 
-          type="submit"
-          className={`bg-transparent hover:bg-amber-600 text-amber-500 font-semibold hover:text-white py-2 px-2 sm:px-3 md:px-4 lg:px-5 border border-amber-500 hover:border-transparent rounded
-            ${
-              isGenerating || formData.senderEmail === "" || formData.senderName == ""
-                ? "cursor-not-allowed opacity-50"
-                : ""
-            }`}
-          disabled={isGenerating || formData.senderEmail === "" || formData.senderName == ""}
-        >
-          {isGenerating ? "Sending..." : "Sending Postcard"}
-        </button>
-      </form>
+          <div className="flex flex-col" style={{ color: '#00FF00' }}>
+            Recipient's Name: Governor Ron DeSantis
+          </div>
+          <div className="flex flex-col" style={{ color: '#00FF00' }}>
+            Their Email: GovernorRon.DeSantis@eog.myflorida.com
+          </div>
+          <div className="flex flex-col">
+            <label className="sr-only" htmlFor="extra">Anything you want to ADD? (Optional)</label>
+            <textarea
+              rows={7}
+              maxLength={200}
+              id="extra"
+              name="extra"
+              value={formData.extra}
+              placeholder="Anything you want to ADD? (Optional)"
+              onChange={handleInput}
+              className="px-2 py-0.5 block w-full rounded-md bg-white border border-gray-40 shadow-s focus:outline-none focus:outline-amber-300"
+              style={{ color: '#00FF00', backgroundColor: '#000000', borderColor: '#00FF00' }}
+            />
+            <div className="text-slate-400 text-right py-2" style={{ color: '#00FF00' }}>{characterCount} / 200</div>
+          </div>
+          <button 
+            type="submit"
+            className={`bg-transparent hover:bg-amber-600 text-amber-500 font-semibold hover:text-white py-2 px-2 sm:px-3 md:px-4 lg:px-5 border border-amber-500 hover:border-transparent rounded
+              ${
+                isGenerating || formData.senderEmail === "" || formData.senderName == ""
+                  ? "cursor-not-allowed opacity-50"
+                  : ""
+              }`}
+            disabled={isGenerating || formData.senderEmail === "" || formData.senderName == ""}
+            style={{ color: '#00FF00', borderColor: '#00FF00' }}
+          >
+            {isGenerating ? "Sending..." : "Send Postcard"}
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
   )
 }
+
