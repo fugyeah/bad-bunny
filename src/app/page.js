5222f88de46a474251463a1a3a563792dbee64f3
Fugyeah <30422405+fugyeah@users.noreply.github.com>
2023-08-03 07:48:05 -0400

    Change to old style

--- src/app/page.js
+++ src/app/page.js
@@ -3,101 +3,48 @@ import Form from './components/main-form';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-12 lg:p-24">
-         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-2xl text-center">
-  Send a e-postcard to the Governor telling him how much we miss him in Florida!</p>
-
-        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        </div>
-
-      </div>        
-           <div className="w-full flex items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black mb-4 md:mt-24 lg:mt-0">
-        <a
-          className="flex place-items-center gap-2 p-8"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-            Step 1{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Put in your name.
-          </p>
-      
-        <a
-          href=""
-          className="group rounded-lg border border-transparent p-2 sm:p-3 md:p-4 lg:p-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Step 2{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Put in your email.
-          </p>
-        </a>
-
-        <a
-          href=""
-          className="group rounded-lg border border-transparent p-2 sm:p-3 md:p-4 lg:p-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Optional{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Put in additional information for why you miss him.  Remember to stay positive!
-          </p>
-        </a>
-
-        <a
-          href=""
-          className="group rounded-lg border border-transparent p-2 sm:p-3 md:p-4 lg:p-5 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Send Postcard{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            The Governor will receive an email generated postcard from chatGPT
-          </p>
-      </div>
-          </a>
-      <div className="p-8 bg-transparent w-full sm:w-3/4 md:w-1/2 lg:w-1/3 object-contain mx-auto">
-        <Image
-          src="/WishYouWereHere.png"
-          alt="Wish you were Here"
-          width={897}
-          height={119}
-          priority
-        />
-      </div>
-      <div className="w-full sm:w-10/12 md:w-10/12 lg:w-10/12">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1 style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'center' }}>
+        <pre>
+        {`  _    _ ______ _      _      ____  ______ _               _ 
+  | |  | |  ____| |    | |    / __ \\|  ____| |        /\\   | |
+  | |__| | |__  | |    | |   | |  | | |__  | |       /  \\  | |
+  |  __  |  __| | |    | |   | |  | |  __| | |      / /\\ \\ | |
+  | |  | | |____| |____| |___| |__| | |    | |____ / ____ \\|_|
+  |_|  |_|______|______|______\\____/|_|    |______/_/    \\_(_)`}
+        </pre>
+      </h1>
+      <h1 style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'center' }}>
+         Send a e-postcard to the Governor telling him how much we miss him in Florida!
+      </h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '10px' }}>
+  <Image
+    src="/WishYouWereHere.png"
+    alt="Wish you were Here"
+    width={300}
+    height={40}
+    priority
+  />
+  <div style={{ marginLeft: '10px', width: '400px', backgroundColor: '#000000', padding: '10px', borderRadius: '10px', boxShadow: '0px 0px 20px rgba(0,0,0,0.5)', textAlign: 'left', borderBottom: '2px solid #00FF00' }}>
+    <h1 style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'Center' }}>E-Postcard Instructions!</h1>
+    <p style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}><b>Step 1:</b> Submit your name.</p>
+    <p style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}><b>Step 2:</b> Enter your e-mail.</p>
+    <p style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}><b>Optional:</b> Put in additional information for why you miss him.  Remember to stay positive!</p>
+    <p style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}><b>Send Postcard:</b> The Governor will receive an email postcard message generated from AI.</p>
+  </div>
+</div>
+       
         <Form />
+
+
+      <div style={{ textAlign: 'center' }}>
+       
+              <p style={{ textAlign: 'center', fontSize:'18px' }}> </p>
+       
+        <a href="https://twitter.com/helloflanews" style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}>Follow me on Twitter!</a>
       </div>
-      <div className="w-full sm:w-10/12 md:w-10/12 lg:w-10/12">
-        A service of HelloFLA as a demonstration of web app that automatically customizes each constituent letter on your issue advocacy campaigns using GPT! Copyright 2023 Painted Dog Productions, LLC
-      </div>
-    </main>
-  )
+      <br />
+      <p style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#00FF00', textAlign: 'left' }}>A service of HelloFLA as a demonstration of web app that automatically customizes each constituent letter on your issue advocacy campaigns using GPT! Copyright 2023 Painted Dog Productions, LLC</p>
+    </div>
+  );
 }
